df18c00ccd2c088746149294f3544ad623ebaae0
Matt Troutman <1972243+trtmn@users.noreply.github.com>
2023-08-25 13:36:38 -0500

    Update plugin.php
    Removed referenced to Linked in and replaced with Masto links. (Specifically hachyderm.io server)

--- share-linkedin/plugin.php
+++ share-linkedin/plugin.php
@@ -1,42 +1,44 @@
 <?php
 /*
-Plugin Name: Share LinkedIn
-Plugin URI: https://github.com/popnt/yourls-linkedin-share
-Description: Adds LinkedIn to the Quick Share Box. 
-Version: 1.0.0.1
-Author: Julia Greaven
-Author URI: //popnt.com
+Plugin Name: Share Mastodon
+Plugin URI: https://github.com/geniuslounge/yourls-masto-share
+Forked From: https://github.com/popnt/yourls-linkedin-share
+Description: Adds Mastodon to the Quick Share Box. 
+Version: 1.0
+Author: Matt Troutman
+Author URI: https://trtmn.com
 */
 
 
-yourls_add_action( 'share_links', 'linkedin_share_url' );
-function linkedin_share_url( $args ) {
+yourls_add_action( 'share_links', 'mastodon_share_url' );
+function mastodon_share_url( $args ) {
     list( $longurl, $shorturl, $title, $text ) = $args;
     $shorturl = rawurlencode( $shorturl );
     $title = rawurlencode( htmlspecialchars_decode( $title ) );
+    $toot_text = $title . ' ' . $shorturl ;
     
     // Plugin URL (no URL is hardcoded)
     $pluginurl = YOURLS_PLUGINURL . '/'.yourls_plugin_basename( dirname(__FILE__) );
-    $icon = $pluginurl.'/linkedin.png';
-    echo <<<LINKEDIN
+    $icon = $pluginurl.'/mastodon.png';
+    echo <<<Mastodon
     <style type="text/css">
-    #share_linkedin{
+    #share_mastodon{
         background: transparent url("$icon") left center no-repeat;
     }
     </style>
-    <a id="share_linkedin"
-        href="https://www.linkedin.com/shareArticle?url=$shorturl&title=$title"
-        title="Share on LinkedIn"
-        onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;">LinkedIn
+    <a id="share_mastodon"
+        href="https://hachyderm.io/share?text=$toot_text"
+        title="Share on Mastodon"
+        onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;">Mastodon
     </a>
     <script type="text/javascript">
-    // Dynamically update LinkedIn link
+    // Dynamically update Masto link
     // when user clicks on the "Share" Action icon, event $('#tweet_body').keypress() is fired, so we'll add to this
     $('#tweet_body').keypress(function(){
         var url = encodeURIComponent( $('#copylink').val() );
-        var linkedin = 'https://www.linkedin.com/shareArticle?url='+url;
-        $('#share_linkedin').attr('href', linkedin);        
+        var mastodon = 'https://hachyderm.io/share?text='+url;
+        $('#share_mastodon').attr('href', mastodon);        
     });
     </script>
-LINKEDIN;
+MASTODON;
 }
