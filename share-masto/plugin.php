6fb02fc33c1f1988b4627adce3a77d51d01a0921
Matt Troutman <1972243+trtmn@users.noreply.github.com>
2023-08-25 14:13:57 -0500

    Update plugin.php
    debugged and fixed issues. 🙂

--- share-masto/plugin.php
+++ share-masto/plugin.php
@@ -2,33 +2,32 @@
 /*
 Plugin Name: Share Mastodon
 Plugin URI: https://github.com/geniuslounge/yourls-masto-share
-Forked From: https://github.com/popnt/yourls-linkedin-share
-Description: Adds Mastodon to the Quick Share Box. 
-Version: 1.0
+Description: Adds Mastodon to the Quick Share Box.
+Version: 1.0.1
 Author: Matt Troutman
 Author URI: https://trtmn.com
 */
 
 
-yourls_add_action( 'share_links', 'mastodon_share_url' );
-function mastodon_share_url( $args ) {
+yourls_add_action( 'share_links', 'masto_share_url' );
+function masto_share_url( $args ) {
     list( $longurl, $shorturl, $title, $text ) = $args;
     $shorturl = rawurlencode( $shorturl );
     $title = rawurlencode( htmlspecialchars_decode( $title ) );
-    $toot_text = $title . ' ' . $shorturl ;
-    
+    $toottext=$title.'%20'.$shorturl;
+
     // Plugin URL (no URL is hardcoded)
     $pluginurl = YOURLS_PLUGINURL . '/'.yourls_plugin_basename( dirname(__FILE__) );
     $icon = $pluginurl.'/mastodon.svg';
-    echo <<<Mastodon
+    echo <<<MASTO
     <style type="text/css">
-    #share_mastodon{
+    #share_linkedin{
         background: transparent url("$icon") left center no-repeat;
     }
     </style>
-    <a id="share_mastodon"
-        href="https://hachyderm.io/share?text=$toot_text"
-        title="Share on Mastodon"
+    <a id="share_linkedin"
+        href="https://hachyderm.io/share?text=$toottext"
+        title="Share on Masto"
         onclick="javascript:window.open(this.href,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;">Mastodon
     </a>
     <script type="text/javascript">
@@ -36,9 +35,10 @@ function mastodon_share_url( $args ) {
     // when user clicks on the "Share" Action icon, event $('#tweet_body').keypress() is fired, so we'll add to this
     $('#tweet_body').keypress(function(){
         var url = encodeURIComponent( $('#copylink').val() );
-        var mastodon = 'https://hachyderm.io/share?text='+url;
-        $('#share_mastodon').attr('href', mastodon);        
+        var masto_body = $('#tweet_body').val();
+        var masto = 'https://hachyderm.io/share?text='+masto_body;
+        $('#share_linkedin').attr('href', masto);
     });
     </script>
-MASTODON;
+MASTO;
 }
